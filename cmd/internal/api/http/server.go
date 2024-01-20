80e689fd8d4416186da5681d5a1ccfdf5fc7a485
Andries de Kock <andries.de@gmail.com>
2024-01-20 20:47:49 +0200

    Add HTTP server and utility functions, implemented fact endpoint

--- cmd/internal/api/http/server.go
+++ cmd/internal/api/http/server.go
@@ -1,1 +1,47 @@
 package server
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/aidk/gocatfacts/cmd/internal/api/service"
+	"github.com/aidk/gocatfacts/cmd/internal/api/util"
+)
+
+// Server is the interface that provides the HTTP server.
+type Server struct {
+	svc service.Service
+}
+
+// NewServer returns a new Server. It requires a Service. It returns a pointer to a Server.
+func NewServer(svc service.Service) *Server {
+	return &Server{
+		svc: svc,
+	}
+}
+
+// Start starts the HTTP server on the specified port.
+func (s *Server) Start(port string) error {
+
+	// we register our handler on the default router
+	http.HandleFunc("/fact", s.handleGetFact)
+
+	// we start the HTTP server
+	return http.ListenAndServe(port, nil)
+}
+
+// handleGetFact is the handler for the GET /fact endpoint.
+func (s *Server) handleGetFact(w http.ResponseWriter, r *http.Request) {
+
+	// we get a fact from the service
+	fact, err := s.svc.GetFact(context.Background())
+	if err != nil {
+		// if there was an error, we write the error to the response body
+		util.WriteToJSON(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// we write the fact to the response body
+	util.WriteToJSON(w, http.StatusOK, fact)
+
+}
--- cmd/internal/api/util/util.go
+++ cmd/internal/api/util/util.go
@@ -1,1 +1,19 @@
 package util
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// WriteToJSON writes the value as JSON to the response writer
+func WriteToJSON(w http.ResponseWriter, s int, v any) error {
+
+	// Set the Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Set the status code for the request
+	w.WriteHeader(s)
+
+	// Encode the value v as JSON and write it to the response
+	return json.NewEncoder(w).Encode(v)
+}
